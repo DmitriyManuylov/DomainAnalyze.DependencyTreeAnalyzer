896ef741e26f3878db56ed7b262d4d7caac907fb
Dmitriy Manuylov <dmitriy.a.manuylov@yandex.ru>
2026-08-11 23:34:53 +0300

    Поиск вызовов методов зависимостей

--- DomainAnalyze.DependencyTree/Services/DependencyTreeAnalyzer.cs
+++ DomainAnalyze.DependencyTree/Services/DependencyTreeAnalyzer.cs
@@ -27,6 +27,7 @@ namespace DomainAnalyze.DependencyTree.Services
         private HashSet<INamedTypeSymbol> ProcessedInjections = new HashSet<INamedTypeSymbol>(NamedTypeEqualityComparer.Instance);
         protected NamedTypeSymbolTree SymbolTree { get; private init; }
         protected Dictionary<IPropertySymbol, IFieldSymbol> PropertiesToFieldsMapping = new Dictionary<IPropertySymbol, IFieldSymbol>(PropertySymbolEqualityComparer.Instance);
+        protected Dictionary<IFieldSymbol, IInvocationOperation> FieldsInvocations = new Dictionary<IFieldSymbol, IInvocationOperation>(FieldSymbolEqualityComparer.Instance);
 
         protected Dictionary<INamedTypeSymbol, List<INamedTypeSymbol>> InterfacesImplementations = new Dictionary<INamedTypeSymbol, List<INamedTypeSymbol>>(NamedTypeEqualityComparer.Instance);
         private List<IDependencyRegistrationsSearcher> DependencyRegistrationSearchersList = new List<IDependencyRegistrationsSearcher>();
@@ -80,6 +81,8 @@ namespace DomainAnalyze.DependencyTree.Services
 
                 PropertiesToFieldsMapping.TryAdd(prop, fieldNode.FieldSymbol);
             }
+
+            SearchInvocations();
 
             await InnerAnalyze();
 
@@ -264,6 +267,40 @@ namespace DomainAnalyze.DependencyTree.Services
             }
 
             return dependencyRegistrations;
+        }
+
+        private void SearchInvocations()
+        {
+            foreach (var field in SymbolTree.Fields)
+            {
+                var propertiesRefInvocations = field
+                    .ContainingTypeNode
+                    .Symbol
+                    .GetMembers()
+                    .OfType<IPropertyReferenceOperation>()
+                    .Where(item => PropertiesToFieldsMapping.TryGetValue(item.Property, out var outField) && FieldSymbolEqualityComparer.Instance.Equals(outField, field.FieldSymbol))
+                    .Select(item => item.Parent as IInvocationOperation)
+                    .Where(item => item is not null);
+
+                foreach (var inv in propertiesRefInvocations)
+                {
+                    FieldsInvocations.TryAdd(field.FieldSymbol, inv);
+                }
+
+                var fieldsRefInvocations = field
+                    .ContainingTypeNode
+                    .Symbol
+                    .GetMembers()
+                    .OfType<IFieldReferenceOperation>()
+                    .Where(item => FieldSymbolEqualityComparer.Instance.Equals(item.Field, field.FieldSymbol))
+                    .Select(item => item.Parent as IInvocationOperation)
+                    .Where(item => item is not null); ;
+
+                foreach(var inv in fieldsRefInvocations)
+                {
+                    FieldsInvocations.TryAdd(field.FieldSymbol, inv);
+                }
+            }
         }
 
         public void SetDependencyRegistrationsSearcher<TType>() where TType : class, IDependencyRegistrationsSearcher, new()
--- DomainAnalyze.DependencyTree/Utilities/Extensions/FieldSymbolExtensions.cs
+++ DomainAnalyze.DependencyTree/Utilities/Extensions/FieldSymbolExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using DomainAnalyze.DependencyTree.Utilities.EqualityComparers;
+using Microsoft.CodeAnalysis;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,6 +13,11 @@ namespace DomainAnalyze.DependencyTree.Utilities.Extensions
         public static IPropertySymbol GetAssociatedProperty(this IFieldSymbol fieldSymbol)
         {
             return fieldSymbol.AssociatedSymbol as IPropertySymbol;
+        }
+
+        public static bool CustomEquals(this IFieldSymbol fieldSymbol1, IFieldSymbol fieldSymbol2)
+        {
+            return FieldSymbolEqualityComparer.Instance.Equals(fieldSymbol1, fieldSymbol2);
         }
     }
 }
